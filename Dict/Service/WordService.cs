05f9cfbffd05b4e57af507882cc98d453380c1bc
Do Van Duc Anh <dovanducanh06@gmail.com>
2025-09-13 14:55:41 +0700

    add search word

---
+++ Dict/Service/WordService.cs
@@ -0,0 +1,25 @@
+﻿using Dict.Data;
+using Dict.Service.IService;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dict.Service
+{
+    public class WordService : IWordService
+    {
+        private readonly ApplicationDbContext _db;
+
+        public WordService(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<string?> GetWordJson(string label)
+        {
+            return await _db.Entries
+                .AsNoTracking()
+                .Where(k => k.Type == "word" && k.Label == label)
+                .Select(k => k.RawJson)
+                .FirstOrDefaultAsync();
+        }
+    }
+}
