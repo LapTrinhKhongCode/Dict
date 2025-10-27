371eb8f451fe14bb62f532e27b293dda6c78d1e2
Dungsora04 <tuandung040204@gmail.com>
2025-10-27 23:05:55 +0700

    fix api reference

--- frontend/app/utils/api.ts
+++ frontend/app/utils/api.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = process.env.API_BASE_URL || 'https://localhost:7084';
+export const API_BASE_URL = process.env.NUXT_PUBLIC_API_BASE_URL || 'https://localhost:7084';
 
 export function apiUrl(path: string) {
   return `${API_BASE_URL}${path.startsWith('/') ? path : `/${path}`}`
